1e6cfc99d29dad3237cbcd93a6a49bf9d857861f
diegofrsilva <diegofrsilva@gmail.com>
2016-12-10 13:04:15 -0200

    arquivos segunda aula

---
+++ curso-java-aula2/src/test/java/br/com/dextraining/test/ExemploTest.java
@@ -0,0 +1,20 @@
+package br.com.dextraining.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExemploTest {
+
+	@Test
+	public void testarSoma() {
+		int valor1 = 10;
+		int valor2 = 20;
+		int resultado = valor1 + valor2;
+
+		Assert.assertEquals(30, resultado);
+	}
+
+	@Test
+	public void testarSubtracao() {
+	}
+}
